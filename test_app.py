e3b7405d5fc5597e7fc45704b14b81b266d482e5
berrie-be <82584598+berrie-be@users.noreply.github.com>
2026-01-20 08:30:52 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,23 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_route(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hello Flask"
+
+
+def test_health_route(client):
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json == {"status": "ok"}
